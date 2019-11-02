324b84e3a6ddca22d370408f865fe4fb3340175d
Marco Farnezi <marcofarnezi@gmail.com>
2019-11-02 10:59:04 +0100

    refactory

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -19,52 +19,49 @@ module.exports = {
     async store(req, res) {
         const { search } = req.body;
         const domain = "https://www.amazon.com";
-    
-        (async () => {  
-            try {    
-              const browser = await puppeteer.launch({headless: true});    
-              const page = await browser.newPage();    
-              //await page.setViewport({ width: 1280, height: 800 });
-              await page.goto(domain, {
-                timeout: 3000000
-              });
-              
-              await page.type('#twotabsearchtextbox', search);
-              await page.click('input.nav-input');
-              await page.waitForSelector('.s-image');
-              for (let i = 0; i < 2; i++) {                
-                  const products = await page.evaluate(() => {
-                    const links = Array.from(document.querySelectorAll('.s-result-item'));
-                    
-                    return links.map(link => {
-                        if (link.querySelector(".a-price-whole")) {
-                            return {
-                                title: link.querySelector(".a-size-medium.a-color-base.a-text-normal").textContent,
-                                url: link.querySelector(".a-link-normal.a-text-normal").href,                
-                                avg: parseFloat(link.querySelector('.a-icon-star-small > span').textContent.match(/([0-9,.]+) .*/)[1]),
-                                price: parseFloat(link.querySelector(".a-price-whole").textContent.replace(/[,.]/g, m => (m === ',' ? '.' : ''))),
-                            };
-                        }
-                    });
-                  });
-          
-                 products.forEach(async element => { 
-                    if (element != null) {
-                        await Product.create(element);
+        
+        try {    
+            const browser = await puppeteer.launch({headless: true});    
+            const page = await browser.newPage();    
+            await page.setViewport({ width: 1280, height: 800 });
+            await page.goto(domain, {
+            timeout: 3000000
+            });
+            
+            await page.type('#twotabsearchtextbox', search);
+            await page.click('input.nav-input');
+            await page.waitForSelector('.s-image');
+            for (let i = 0; i < 2; i++) {                
+                const products = await page.evaluate(() => {
+                const links = Array.from(document.querySelectorAll('.s-result-item'));
+                
+                return links.map(link => {
+                    if (link.querySelector(".a-price-whole")) {
+                        return {
+                            title: link.querySelector(".a-size-medium.a-color-base.a-text-normal").textContent,
+                            url: link.querySelector(".a-link-normal.a-text-normal").href,                
+                            avg: parseFloat(link.querySelector('.a-icon-star-small > span').textContent.match(/([0-9,.]+) .*/)[1]),
+                            price: parseFloat(link.querySelector(".a-price-whole").textContent.replace(/[,.]/g, m => (m === ',' ? '.' : ''))),
+                        };
                     }
-                  }); 
+                });
+                });
                   
-                  await page.waitForSelector('.a-normal');
-                  await page.click('.a-normal');
-                  await page.waitForSelector('.s-image');
-              }
-        
-              await browser.close();
-              return res.json(products);
-            } catch (error) {
-                return res.json(error);
+                products.forEach(async element => { 
+                if (element != null) {
+                    await Product.create(element);
+                }
+                }); 
+                
+                await page.waitForSelector('.a-normal');
+                await page.click('.a-normal');
+                await page.waitForSelector('.s-image');
             }
-          })();
-        
+
+            await browser.close();
+            return res.json({loaded : true});
+        } catch (error) {
+            return res.json(error);
+        }    
     } 
 };
